3a96907c42c54ed80e961347763edafc27501f94
Zomara <zachary.omara@gmail.com>
2025-03-16 16:52:35 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+#https://docs.streamlit.io/develop/api-reference
+
+st.title("Title")
+st.divider()
+st.header("header")
+st.subheader("subheader")
+st.caption("caption")
+st.color_picker("Colors")
+st.feedback("stars")
